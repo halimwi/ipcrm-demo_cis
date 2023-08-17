443a4d17963ff8e8769eb471f704c4e35e99002d
halimwi <hwijaya@gmail.com>
2023-08-17 17:36:52 +0800

    Update installwireshark.ps1

--- tasks/installwireshark.ps1
+++ tasks/installwireshark.ps1
@@ -6,6 +6,6 @@ echo "Started Wireshark installation"
 Start-Process('C:\temp\Wireshark-win64-4.0.7.exe') -ArgumentList '/S'
 echo "Installing Wireshark"
 sleep 15
-echo "Validating Wireshark installation ..."
+echo "Validating Wireshark installation, checking registry key to confirm wireshark installation is successful"
 get-itemproperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' | findstr wireshark
 get-itemproperty 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | findstr wireshark
